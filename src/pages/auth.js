7a22185baf31f7b837f739614bc61512edbb8a18
877wrangler <877wrangler@gmail.com>
2023-03-20 22:22:39 -0400

    Introduce some CSS and register component

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -1,3 +1,42 @@
+import { useState } from "react";
+
 export const Auth = () => {
-    return <div> Auth </div>
+    return (
+    <div className="auth">
+        <Login />
+        <Register />
+    </div>
+    );
+};
+
+const Login = () => {
+
+};
+
+const Register = () => {
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    return ( 
+    <div>
+        <h2> Register </h2>
+        <div className="form-group">
+            <label htmlFor="username"> Username: </label>
+            <input 
+                type="text" 
+                id="username"
+                value={username} 
+                onChange={(event) => setUsername(event.target.value)}/>
+        </div>
+        <div className="form-group">
+            <label htmlFor="password"> Password: </label>
+            <input 
+                type="text" 
+                id="password" 
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}/>
+        </div>
+    </div>
+    );
 };
